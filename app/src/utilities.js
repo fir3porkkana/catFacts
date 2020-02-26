1e7ecf45788b35c3e41bd807e6e805b4d53ec364
Tiera Salmi <tiera.salmi@gmail.com>
2020-02-26 19:32:52 +0200

    added prevention for infinite loop in selecting 5 random facts, in case the server returns 4 or fewer facts

--- app/src/utilities.js
+++ app/src/utilities.js
@@ -1,12 +1,32 @@
 const selectRandom = (allFacts) => {
     let factList = []
-    let i = 0;
-    while (i < 5) {
+    for (let i = 0; i < allFacts.length; i++) {
         const index = Math.floor(Math.random() * Math.floor(allFacts.length))
         if (!factList.includes(allFacts[index])) {
             factList.push(allFacts[index])
-            i++;
         }
+
+        if (factList.length === 5) {
+            break
+        }
+    }
+
+    if (factList.length === 0) {
+        const defaultFact = {
+            _id: "58e008ad0aac31001185ed0c",
+            text: "The frequency of a domestic cat's purr is the same at which muscles and bones repair themselves.",
+            type: "cat",
+            user: {
+                _id: "58e007480aac31001185ecef",
+                name: {
+                    first: "Kasimir",
+                    last: "Schulz"
+                }
+            },
+            upvotes: 9,
+            userUpvoted: null
+        }
+        factList.push(defaultFact)
     }
     return factList
 }
