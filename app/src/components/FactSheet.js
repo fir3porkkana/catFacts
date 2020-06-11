98d570262f33922f8f8feb269ed7cf392bcbdedd
Tiera Salmi <tiera.salmi@gmail.com>
2020-06-11 17:23:28 +0300

    adding script for vercel setup

--- app/src/components/FactSheet.js
+++ app/src/components/FactSheet.js
@@ -3,12 +3,12 @@ import { Card } from "semantic-ui-react";
 
 const FactSheet = ({ fact }) => {
 
-    return (
-        <Card
-            description={fact.text}
-            fluid
-        />
-    )
+  return (
+    <Card
+      description={fact.text}
+      fluid
+    />
+  )
 }
 
 export default FactSheet
